a80c905f4e688f1ddbf66044e9759b0e9d90551e
chengyuxing <chengyuxingo@gmail.com>
2020-09-27 16:28:01 +0800

    优化表达式解析逻辑，提高容错率，增加支持括号，取反，静态表达式

--- src/main/java/rabbit/common/types/CExpression.java
+++ src/main/java/rabbit/common/types/CExpression.java
@@ -1,10 +1,8 @@
 package rabbit.common.types;
 
-import rabbit.common.tuple.Pair;
-import rabbit.common.utils.StringUtil;
-
-import java.util.ArrayList;
-import java.util.List;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,13 +16,13 @@ import java.util.regex.Pattern;
  * 支持的逻辑运算符: {@code &&, ||}<br>
  * e.g.
  * <blockquote>
- * {@code :id >= 0 && :name <> blank && :age<=21}
+ * {@code !(:id >= 0 || :name <> blank) && :age<=21}
  * </blockquote>
  */
 public class CExpression {
     public static final String NUMBER_REGEX = "-?([0-9]|(0\\.\\d+)|([1-9]+\\.?\\d+))";
-    private static final Pattern filterPattern = Pattern.compile(":(?<name>\\w+) *(?<op>[><=!@~]{1,2}) *(?<value>[\\S\\s]+)");
-
+    private static final Pattern FILTER_PATTERN = Pattern.compile("\\s*:(?<name>\\w+)\\s*(?<op>[><=!@~]{1,2})\\s*(?<value>\\w+|'[^']*'|\"[^\"]*\"|-?[.\\d]+)\\s*");
+    private static final ScriptEngine SCRIPT_ENGINE = new ScriptEngineManager().getEngineByName("nashorn");
     private final String expression;
 
     /**
@@ -40,7 +38,7 @@ public class CExpression {
      * 创建一个表达式实例<br>
      * e.g.
      * <blockquote>
-     * {@code :id >= 0 && :name <> blank && :age<=21}
+     * {@code !(:id >= 0 || :name <> blank) && :age<=21}
      * </blockquote>
      *
      * @param expression 表达式
@@ -56,65 +54,41 @@ public class CExpression {
      * @param args 参数字典
      * @return 逻辑运算的结果
      */
-    public boolean getResult(Map<String, Object> args) {
-        Pair<List<String>, List<String>> ps = StringUtil.split(expression, "(?<op>\\|\\||&&) *[^ '\"]", "op");
-
-        List<String> filters = ps.getItem1();
-        List<String> operators = ps.getItem2();
-
-        List<Boolean> results = new ArrayList<>();
-        for (String filter : filters) {
-            Matcher m = filterPattern.matcher(filter.trim());
-            if (m.matches()) {
-                String name = m.group("name");
-                String op = m.group("op");
-                String value = m.group("value");
-                boolean itemResult = compare(name, op, value, args);
-                results.add(itemResult);
-            } else {
-                throw new IllegalArgumentException("child expression format error: " + filter);
-            }
+    public boolean calc(Map<String, Object> args) {
+        try {
+            return calc(expression, args);
+        } catch (ScriptException e) {
+            throw new ArithmeticException("expression script syntax error：" + e.getMessage());
         }
-        return boolCalc(results, operators);
     }
 
     /**
-     * 一组布尔运算
+     * 解析计算表达式
      *
-     * @param bools          一组布尔值
-     * @param logicalSymbols 一组布尔值之间的逻辑连接符号
+     * @param expression 一组布尔值
+     * @param args       参数字典
      * @return 运算后的布尔结果
      */
-    public static boolean boolCalc(List<Boolean> bools, List<String> logicalSymbols) {
-        if (bools.size() - logicalSymbols.size() != 1) {
-            throw new IllegalArgumentException("logical error, bools size must greater than logicalSymbols size of 1");
+    private static boolean calc(String expression, Map<String, Object> args) throws ScriptException {
+        Matcher m = FILTER_PATTERN.matcher(expression);
+        if (m.find()) {
+            String filter = m.group(0);
+            String name = m.group("name");
+            String op = m.group("op");
+            String value = m.group("value");
+            boolean bool = CExpression.compare(name, op, value, args);
+            return calc(expression.replace(filter, bool + ""), args);
         }
-        if (bools.size() == 1) {
-            return bools.get(0);
-        }
-        if (logicalSymbols.size() >= 1) {
-            boolean prev = bools.get(0);
-            boolean next = bools.get(1);
-            String op = logicalSymbols.get(0);
-            if (op.equals("&&")) {
-                bools.remove(0);
-                bools.set(0, prev && next);
-            } else if (op.equals("||")) {
-                bools.remove(0);
-                bools.set(0, prev || next);
-            }
-            logicalSymbols.remove(0);
-        }
-        return boolCalc(bools, logicalSymbols);
+        return Boolean.parseBoolean(SCRIPT_ENGINE.eval(expression).toString());
     }
 
     /**
      * 值对比
      *
-     * @param name   对应参数字典的key名
-     * @param op     操作符
-     * @param value  被比对的值
-     * @param args 参数字典
+     * @param name  对应参数字典的key名
+     * @param op    操作符
+     * @param value 被比对的值
+     * @param args  参数字典
      * @return 比较结果
      */
     private static boolean compare(String name, String op, String value, Map<String, Object> args) {
@@ -137,10 +111,10 @@ public class CExpression {
     /**
      * 对比数字类型
      *
-     * @param name   对应参数字典的key名
-     * @param op     操作符
-     * @param value  被比对的值
-     * @param args 参数字典
+     * @param name  对应参数字典的key名
+     * @param op    操作符
+     * @param value 被比对的值
+     * @param args  参数字典
      * @return 比较结果
      */
     private static boolean compareNumber(String name, String op, String value, Map<String, Object> args) {
@@ -169,10 +143,10 @@ public class CExpression {
     /**
      * 对比非数字类型
      *
-     * @param name   对应参数字典的key名
-     * @param op     操作符
-     * @param value  被比对的值
-     * @param args 参数字典
+     * @param name  对应参数字典的key名
+     * @param op    操作符
+     * @param value 被比对的值
+     * @param args  参数字典
      * @return 比较结果
      */
     private static boolean compareNonNumber(String name, String op, String value, Map<String, Object> args) {
@@ -185,13 +159,13 @@ public class CExpression {
             case "<>":
                 return !equal(source, value);
             case "~":
-                return regex(source, value, false);
+                return regexMatch(source, value, false);
             case "!~":
-                return !regex(source, value, false);
+                return !regexMatch(source, value, false);
             case "@":
-                return regex(source, value, true);
+                return regexMatch(source, value, true);
             case "!@":
-                return !regex(source, value, true);
+                return !regexMatch(source, value, true);
             default:
                 throw new UnsupportedOperationException(String.format("unKnow operation for compare NonNumber: \"%s\" %s %s", source, op, value));
         }
@@ -200,16 +174,16 @@ public class CExpression {
     /**
      * 正则表达式比较
      *
-     * @param source  源值
-     * @param value   正则表达式
-     * @param matches 是否全匹配，否则就是包含关系
+     * @param source    源值
+     * @param value     正则表达式
+     * @param fullMatch 是否全匹配，否则就是包含关系
      * @return 是否满足正则
      */
-    private static boolean regex(Object source, String value, boolean matches) {
+    private static boolean regexMatch(Object source, String value, boolean fullMatch) {
         String regex = getString(value);
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(source.toString());
-        if (matches) {
+        if (fullMatch) {
             return m.matches();
         }
         return m.find();
---
+++ src/test/java/tests/ck/ExpressionTest.java
@@ -0,0 +1,29 @@
+package tests.ck;
+
+import org.junit.Test;
+import rabbit.common.types.CExpression;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExpressionTest {
+
+    @Test
+    public void e() throws Exception {
+        String expression = "(:id == 1 && :name == 'json') || :id == 3";
+        String expression2 = "!(   :id ==       -0.25 && :name == '我的') || (:id == 3 && :name == 'mike')";
+
+        Map<String, Object> args = new HashMap<>();
+        args.put("id", 3);
+        args.put("name", "mike");
+
+        CExpression exp = CExpression.of(expression2);
+        System.out.println(exp.calc(args));
+
+    }
+
+    @Test
+    public void num() throws Exception {
+        System.out.println("-0.09".matches(CExpression.NUMBER_REGEX));
+    }
+}
