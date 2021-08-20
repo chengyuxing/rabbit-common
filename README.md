0dcab7e7dd90f7971dc8c443218229136ba255d1
chengyuxing <chengyuxingo@gmail.com>
2021-08-20 17:49:45 +0800

    增加io流输出通用类

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
   <dependency>
       <groupId>com.github.chengyuxing</groupId>
       <artifactId>rabbit-common</artifactId>
-      <version>2.0.7</version>
+      <version>2.0.8</version>
   </dependency>
   ```
 
---
+++ src/main/java/com/github/chengyuxing/common/io/IOutput.java
@@ -0,0 +1,76 @@
+package com.github.chengyuxing.common.io;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * 数据输出帮助通用接口
+ */
+public interface IOutput {
+    /**
+     * 输出到字节数据
+     *
+     * @return 字节数据
+     * @throws IOException 存储字节发生移除
+     */
+    byte[] toBytes() throws IOException;
+
+    /**
+     * 写到输出流
+     *
+     * @param outputStream 输出流
+     * @param close        是否在完成后关闭输出流
+     * @throws IOException ioEx
+     */
+    default void saveTo(OutputStream outputStream, boolean close) throws IOException {
+        outputStream.write(toBytes());
+        if (close) {
+            outputStream.flush();
+            outputStream.close();
+        }
+    }
+
+    /**
+     * 写到输出流并关闭输出流
+     *
+     * @param outputStream 输出流
+     * @throws IOException ioEx
+     */
+    default void saveTo(OutputStream outputStream) throws IOException {
+        saveTo(outputStream, true);
+    }
+
+    /**
+     * 保存到指定路径下
+     *
+     * @param path 文件保存路径（后缀可选）
+     * @throws IOException ioEx
+     */
+    default void saveTo(String path) throws IOException {
+        saveTo(new FileOutputStream(path));
+    }
+
+    /**
+     * 保存到文件对象
+     *
+     * @param file 文件对象
+     * @throws IOException ioEx
+     */
+    default void saveTo(File file) throws IOException {
+        saveTo(new FileOutputStream(file));
+    }
+
+    /**
+     * 保存到路径对象
+     *
+     * @param path 路径对象
+     * @throws IOException ioEx
+     */
+    default void saveTo(Path path) throws IOException {
+        saveTo(Files.newOutputStream(path));
+    }
+}
