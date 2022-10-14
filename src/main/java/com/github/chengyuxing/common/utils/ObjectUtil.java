b5feed6d9044469baed7c042bac8eb0fcaba2079
chengyuxing <chengyuxingo@gmail.com>
2022-10-14 22:51:04 +0800

    增加map忽略大小写取值和查找

--- src/main/java/com/github/chengyuxing/common/utils/ObjectUtil.java
+++ src/main/java/com/github/chengyuxing/common/utils/ObjectUtil.java
@@ -3,6 +3,7 @@ package com.github.chengyuxing.common.utils;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Optional;
 
@@ -138,5 +139,57 @@ public final class ObjectUtil {
      */
     public static <T> T nullable(T value, T other) {
         return Optional.ofNullable(value).orElse(other);
+    }
+
+    /**
+     * map是否忽略大小写包含指定的key
+     *
+     * @param map map
+     * @param key 需要查找的key
+     * @param <V> map值类型参数
+     * @return 是否包含
+     */
+    public static <V> boolean containsKeyIgnoreCase(Map<String, V> map, String key) {
+        Iterator<Map.Entry<String, V>> i = map.entrySet().iterator();
+        if (key == null) {
+            while (i.hasNext()) {
+                Map.Entry<String, V> e = i.next();
+                if (e.getKey() == null)
+                    return true;
+            }
+        } else {
+            while (i.hasNext()) {
+                Map.Entry<String, V> e = i.next();
+                if (key.equalsIgnoreCase(e.getKey()))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * map的key忽略大小写获取一个值
+     *
+     * @param map map
+     * @param key 需要取值的key
+     * @param <V> 值类型参数
+     * @return 值
+     */
+    public static <V> V getValueIgnoreCase(Map<String, V> map, String key) {
+        Iterator<Map.Entry<String, V>> i = map.entrySet().iterator();
+        if (key == null) {
+            while (i.hasNext()) {
+                Map.Entry<String, V> e = i.next();
+                if (e.getKey() == null)
+                    return e.getValue();
+            }
+        } else {
+            while (i.hasNext()) {
+                Map.Entry<String, V> e = i.next();
+                if (key.equalsIgnoreCase(e.getKey()))
+                    return e.getValue();
+            }
+        }
+        return null;
     }
 }
