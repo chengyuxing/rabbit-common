f722796fbb37ef095e2842ca70f02ee115e7f16d
chengyuxing <chengyuxingo@gmail.com>
2023-07-25 15:58:07 +0800

    new pipe 'pairs'

--- src/main/java/com/github/chengyuxing/common/script/IPipe.java
+++ src/main/java/com/github/chengyuxing/common/script/IPipe.java
@@ -1,5 +1,12 @@
 package com.github.chengyuxing.common.script;
 
+import com.github.chengyuxing.common.tuple.Pair;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 值转换管道基础接口
  *
@@ -41,6 +48,21 @@ public interface IPipe<T> {
         @Override
         public String transform(Object value) {
             return value.toString().toLowerCase();
+        }
+    }
+
+    /**
+     * Map转为元组集合 [(item1 -> key, item2 -> value), ...]
+     */
+    class Map2Pairs implements IPipe<List<Pair<String, Object>>> {
+        @Override
+        public List<Pair<String, Object>> transform(Object value) {
+            if (value instanceof Map) {
+                List<Pair<String, Object>> list = new ArrayList<>();
+                ((Map<?, ?>) value).forEach((k, v) -> list.add(Pair.of(k.toString(), v)));
+                return list;
+            }
+            return Collections.emptyList();
         }
     }
 }
