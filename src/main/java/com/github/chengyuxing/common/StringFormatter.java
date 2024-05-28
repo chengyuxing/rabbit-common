a39d1e73f5dcfcc1537776263b84d11b27cfe5af
chengyuxing <chengyuxingo@gmail.com>
2024-05-28 14:51:32 +0800

    optimized.

--- src/main/java/com/github/chengyuxing/common/StringFormatter.java
+++ src/main/java/com/github/chengyuxing/common/StringFormatter.java
@@ -6,6 +6,7 @@ import com.github.chengyuxing.common.utils.ObjectUtil;
 import java.util.Map;
 import java.util.Objects;
 import java.util.StringJoiner;
+import java.util.function.BiFunction;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,10 +18,57 @@ public class StringFormatter {
     @SuppressWarnings("UnnecessaryUnicodeEscape")
     private static final char TEMP_HOLDER_PREFIX = '\u0c32';
     //language=RegExp
-    private final Pattern pattern = Pattern.compile("\\$\\{\\s*(?<key>!?" + Patterns.VAR_KEY_PATTERN + ")\\s*}");
+    private static final Pattern pattern = Pattern.compile("\\$\\{\\s*(?<key>!?" + Patterns.VAR_KEY_PATTERN + ")\\s*}");
 
     /**
      * Format string template with variable map.
+     * <p>e.g. template: </p>
+     * <blockquote>
+     * <pre>select ${ fields } from test.user
+     * where ${  cnd}
+     * and id in (${!idArr})
+     * or id = ${!idArr.1}</pre>
+     * </blockquote>
+     * <p>variables: </p>
+     * <blockquote>
+     * <pre>{
+     * fields: "id, name",
+     * cnd: "name = 'cyx'",
+     * idArr: ["a", "b", "c"]
+     * }</pre>
+     * </blockquote>
+     * <p>result: </p>
+     * <blockquote>
+     * <pre>select id, name from test.user
+     * where name = 'cyx'
+     * and id in ('a', 'b', 'c')
+     * or id = 'b'</pre>
+     * </blockquote>
+     *
+     * @param template       string template
+     * @param data           variables
+     * @param valueFormatter function for parse object value to string literal value (value, key name starts with '{@code !}' or not) -&gt; string literal value.
+     * @return formatted string template
+     */
+    public String format(final String template, final Map<String, ?> data, BiFunction<Object, Boolean, String> valueFormatter) {
+        if (Objects.isNull(template)) {
+            return "";
+        }
+        if (template.trim().isEmpty()) {
+            return template;
+        }
+        if (!template.contains("${")) {
+            return template;
+        }
+        if (Objects.isNull(data) || data.isEmpty()) {
+            return template;
+        }
+        return doFormat(template, data, valueFormatter);
+    }
+
+    /**
+     * Format string template with variable map.
+     * <p>If key starts with {@code !}, do quote with {@code ''}.</p>
      * <p>e.g. template: </p>
      * <blockquote>
      * <pre>select ${ fields } from test.user
@@ -48,20 +96,24 @@ public class StringFormatter {
      * @param data     variables
      * @return formatted string template
      */
-    public String format(final String template, final Map<String, ?> data) {
-        if (Objects.isNull(template)) {
-            return "";
-        }
-        if (template.trim().isEmpty()) {
-            return template;
-        }
-        if (!template.contains("${")) {
-            return template;
-        }
-        if (Objects.isNull(data) || data.isEmpty()) {
-            return template;
-        }
-        return doFormat(template, data);
+    public String format(String template, Map<String, ?> data) {
+        return format(template, data, (value, isSpecial) -> {
+            if (value == null) {
+                return "";
+            }
+            Object[] values = ObjectUtil.toArray(value);
+            StringJoiner sb = new StringJoiner(", ");
+            for (Object v : values) {
+                if (v != null) {
+                    String s = v.toString();
+                    if (isSpecial) {
+                        s = "'" + s + "'";
+                    }
+                    sb.add(s);
+                }
+            }
+            return sb.toString();
+        });
     }
 
     /**
@@ -71,7 +123,7 @@ public class StringFormatter {
      * @param data     variables
      * @return formatted string template
      */
-    protected String doFormat(final String template, final Map<String, ?> data) {
+    protected String doFormat(final String template, final Map<String, ?> data, BiFunction<Object, Boolean, String> valueFormatter) {
         String copy = template;
         Matcher m = getPattern().matcher(copy);
         if (m.find()) {
@@ -93,46 +145,21 @@ public class StringFormatter {
                 try {
                     String value;
                     if (isKeyPath) {
-                        value = parseValue(ObjectUtil.getDeepValue(data, key), isSpecial);
+                        value = valueFormatter.apply(ObjectUtil.getDeepValue(data, key), isSpecial);
                     } else {
-                        value = parseValue(data.get(key), isSpecial);
+                        value = valueFormatter.apply(data.get(key), isSpecial);
                     }
                     copy = copy.replace(holder, value);
                 } catch (Exception e) {
                     throw new IllegalArgumentException(e);
                 }
             }
-            return doFormat(copy, data);
+            return doFormat(copy, data, valueFormatter);
         }
         if (copy.lastIndexOf(TEMP_HOLDER_PREFIX) != -1) {
             copy = copy.replace(TEMP_HOLDER_PREFIX, DEFAULT_HOLDER_PREFIX);
         }
         return copy;
-    }
-
-    /**
-     * Parse object value to string literal value.
-     *
-     * @param value     value
-     * @param isSpecial key name starts with '{@code !}' or not
-     * @return string literal value
-     */
-    protected String parseValue(Object value, boolean isSpecial) {
-        if (value == null) {
-            return "";
-        }
-        Object[] values = ObjectUtil.toArray(value);
-        StringJoiner sb = new StringJoiner(", ");
-        for (Object v : values) {
-            if (v != null) {
-                String s = v.toString();
-                if (isSpecial) {
-                    s = "'" + s + "'";
-                }
-                sb.add(s);
-            }
-        }
-        return sb.toString();
     }
 
     /**
