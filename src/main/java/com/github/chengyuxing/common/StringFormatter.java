51b434588375e676e2db5dfc1bd0c12cfb6a1174
chengyuxing <chengyuxingo@gmail.com>
2025-08-13 17:48:18 +0800

    StringFormatter optimized.

--- src/main/java/com/github/chengyuxing/common/StringFormatter.java
+++ src/main/java/com/github/chengyuxing/common/StringFormatter.java
@@ -20,7 +20,6 @@ public class StringFormatter {
     private static final char DEFAULT_HOLDER_PREFIX = '$';
     @SuppressWarnings("UnnecessaryUnicodeEscape")
     private static final char TEMP_HOLDER_PREFIX = '\u0c32';
-    //language=RegExp
     private static final Pattern pattern = Pattern.compile("\\$\\{\\s*(?<key>!?" + Patterns.VAR_KEY_PATTERN + ")\\s*}");
 
     /**
