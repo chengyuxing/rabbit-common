a190206c40926ae4fa82e274e006b26abe22f246
chengyuxing <chengyuxingo@gmail.com>
2025-10-03 21:34:45 +0800

    token optimized.

--- src/main/java/com/github/chengyuxing/common/script/Token.java
+++ src/main/java/com/github/chengyuxing/common/script/Token.java
@@ -3,10 +3,14 @@ package com.github.chengyuxing.common.script;
 public final class Token {
     private final TokenType type;
     private final String value;
+    private final int line;
+    private final int column;
 
-    public Token(TokenType type, String value) {
+    public Token(TokenType type, String value, int line, int column) {
         this.type = type;
         this.value = value;
+        this.line = line;
+        this.column = column;
     }
 
     public TokenType getType() {
@@ -17,11 +21,16 @@ public final class Token {
         return value;
     }
 
+    public int getLine() {
+        return line;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
     @Override
     public String toString() {
-        return "Token{" +
-                "type=" + type +
-                ", value='" + value + '\'' +
-                '}';
+        return type.name() + " (" + value.replace("\n", "\\n") + ") at line " + (line + 1) + ", column " + (column + 1);
     }
 }
--- src/main/java/com/github/chengyuxing/common/script/TokenType.java
+++ src/main/java/com/github/chengyuxing/common/script/TokenType.java
@@ -2,30 +2,42 @@ package com.github.chengyuxing.common.script;
 
 public enum TokenType {
     // Keywords
-    IF, ELSE,
-    ENDIF,    // #fi
-    SWITCH, CASE,
-    CHOOSE, WHEN,
-    DEFAULT, BREAK, END,
-    FOR,
-    END_FOR,   // #done
-    FOR_OF, FOR_DELIMITER, FOR_OPEN, FOR_CLOSE,
-    GUARD,THROW,
+    IF(Directives.IF), ELSE(Directives.ELSE), END_IF(Directives.FI),
+    SWITCH(Directives.SWITCH), CASE(Directives.CASE),
+    CHOOSE(Directives.CHOOSE), WHEN(Directives.WHEN),
+    DEFAULT(Directives.DEFAULT), BREAK(Directives.BREAK), END(Directives.END),
+    FOR(Directives.FOR),
+    FOR_OF("of"), FOR_DELIMITER("delimiter"), FOR_OPEN("open"), FOR_CLOSE("close"),
+    END_FOR(Directives.DONE),
+    GUARD(Directives.GUARD), END_GUARD(Directives.THROW),
+    CHECK(Directives.CHECK), CHECK_THROW("throw"),
 
     // Other tokens
-    IDENTIFIER, STRING, NUMBER,
-    VARIABLE_NAME,  // :name
-    NEWLINE, EOF, UNKNOWN, PLAIN_TEXT, COMMA, COLON,
-    PIPE_SYMBOL,    // |
-    AND_SYMBOL,     // &
+    IDENTIFIER("<identifier>"), STRING("'<string>'"), NUMBER("<number>"),
+    VARIABLE_NAME(":<variable name>"),
+    NEWLINE("\\n"), EOF(""), UNKNOWN("<unknow>"), PLAIN_TEXT("<plain text>"),
+    COMMA(","), COLON(":"),
+    PIPE_SYMBOL("|"),
+    AND_SYMBOL("&"),
 
     // Bool expression tokens
-    LOGIC_OR,   // ||
-    LOGIC_AND,  // &&
-    LOGIC_NOT,   // !
-    OPERATOR,    // > < >= <= = == != <> ~ !~ @ !@
-    LPAREN,     // (
-    RPAREN,      // )
-    LBRACE,     // {
-    RBRACE      // }
+    LOGIC_OR("||"),
+    LOGIC_AND("&&"),
+    LOGIC_NOT("!"),
+    OPERATOR(">, <, >=, <=, =, ==, !=, <>, ~, !~, @, !@"),
+    LPAREN("("),
+    RPAREN(")"),
+    LBRACE("{"),
+    RBRACE("}");
+
+    private final String displayName;
+
+    TokenType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @Override
+    public String toString() {
+        return name() + " (" + displayName + ")";
+    }
 }
