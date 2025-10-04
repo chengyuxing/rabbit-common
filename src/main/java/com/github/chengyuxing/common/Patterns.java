a4c800db8f451c53307bd854b3cc612a5803c720
chengyuxing <chengyuxingo@gmail.com>
2025-10-04 17:11:25 +0800

    Patterns optimized.

---
+++ src/main/java/com/github/chengyuxing/common/Patterns.java
@@ -0,0 +1,9 @@
+package com.github.chengyuxing.common;
+
+public final class Patterns {
+    //language=RegExp
+    /**
+     * e.g. {@code abc a_bc a_1 a.b a.b.c _abc}
+     */
+    public static final String VAR_KEY_PATTERN = "[a-zA-Z_][\\w.]*";
+}
