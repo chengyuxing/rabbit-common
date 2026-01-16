bfb4c3df51458f8c75065862d1dd8e4af5adb8c4
chengyuxing <chengyuxingo@gmail.com>
2026-01-16 16:01:58 +0800

    code optimized.

--- src/main/java/com/github/chengyuxing/common/util/StringUtils.java
+++ src/main/java/com/github/chengyuxing/common/util/StringUtils.java
@@ -90,28 +90,45 @@ public final class StringUtils {
         return Pair.of(sb.toString(), found);
     }
 
+    /**
+     * Checks if the given string starts with any of the specified keywords.
+     *
+     * @param str The string to check.
+     * @param keywords The keywords to look for at the beginning of the string.
+     * @return true if the string starts with any of the keywords, false otherwise.
+     */
     public static boolean startsWiths(String str, String... keywords) {
-        boolean ok = false;
         for (String keyword : keywords) {
             if (str.startsWith(keyword)) {
-                ok = true;
-                break;
+                return true;
             }
         }
-        return ok;
+        return false;
     }
 
+    /**
+     * Checks if the given string ends with any of the specified keywords.
+     *
+     * @param str The string to check.
+     * @param keywords The keywords to look for at the end of the string.
+     * @return true if the string ends with any of the keywords, false otherwise.
+     */
     public static boolean endsWiths(String str, String... keywords) {
-        boolean ok = false;
         for (String keyword : keywords) {
             if (str.endsWith(keyword)) {
-                ok = true;
-                break;
+                return true;
             }
         }
-        return ok;
+        return false;
     }
 
+    /**
+     * Determines if the given string starts with the specified prefix, ignoring case.
+     *
+     * @param str The string to check.
+     * @param starts The prefix to look for at the beginning of the string.
+     * @return true if the string starts with the specified prefix, ignoring case, false otherwise.
+     */
     public static boolean startsWithIgnoreCase(String str, String starts) {
         if (starts.length() > str.length()) {
             return false;
@@ -127,6 +144,13 @@ public final class StringUtils {
         return true;
     }
 
+    /**
+     * Determines if the given string ends with the specified suffix, ignoring case.
+     *
+     * @param str The string to check.
+     * @param ends The suffix to look for at the end of the string.
+     * @return true if the string ends with the specified suffix, ignoring case, false otherwise.
+     */
     public static boolean endsWithIgnoreCase(String str, String ends) {
         if (ends.length() > str.length()) {
             return false;
@@ -143,28 +167,45 @@ public final class StringUtils {
         return true;
     }
 
+    /**
+     * Checks if the given string starts with any of the specified keywords, ignoring case.
+     *
+     * @param str The string to check.
+     * @param keywords The keywords to look for at the beginning of the string.
+     * @return true if the string starts with any of the keywords, ignoring case, false otherwise.
+     */
     public static boolean startsWithsIgnoreCase(String str, String... keywords) {
-        boolean ok = false;
         for (String keyword : keywords) {
             if (startsWithIgnoreCase(str, keyword)) {
-                ok = true;
-                break;
+                return true;
             }
         }
-        return ok;
+        return false;
     }
 
+    /**
+     * Checks if the given string ends with any of the specified keywords, ignoring case.
+     *
+     * @param str The string to check.
+     * @param keywords The keywords to look for at the end of the string.
+     * @return true if the string ends with any of the keywords, ignoring case, false otherwise.
+     */
     public static boolean endsWithsIgnoreCase(String str, String... keywords) {
-        boolean ok = false;
         for (String keyword : keywords) {
             if (endsWithIgnoreCase(str, keyword)) {
-                ok = true;
-                break;
+                return true;
             }
         }
-        return ok;
+        return false;
     }
 
+    /**
+     * Searches for the first occurrence of the specified target string within the source string, ignoring case.
+     *
+     * @param source The string to search within.
+     * @param target The string to search for.
+     * @return The index of the first occurrence of the target string within the source string, or -1 if not found.
+     */
     public static int indexOfIgnoreCase(String source, String target) {
         char[] sourceChars = source.toCharArray();
         char[] targetChars = target.toCharArray();
@@ -196,10 +237,24 @@ public final class StringUtils {
         return -1;
     }
 
+    /**
+     * Checks if the target string is contained within the source string, ignoring case.
+     *
+     * @param source The string to search within.
+     * @param target The string to search for.
+     * @return true if the target string is found within the source string, ignoring case; false otherwise.
+     */
     public static boolean containsIgnoreCase(String source, String target) {
         return indexOfIgnoreCase(source, target) > -1;
     }
 
+    /**
+     * Determines if the source string contains any of the target strings, ignoring case.
+     *
+     * @param source  The string to search within.
+     * @param targets The strings to search for within the source string.
+     * @return true if any of the target strings are found within the source string, ignoring case; false otherwise.
+     */
     public static boolean containsAnyIgnoreCase(String source, String... targets) {
         if (targets.length < 1) {
             return false;
@@ -212,6 +267,13 @@ public final class StringUtils {
         return false;
     }
 
+    /**
+     * Checks if the source string contains any of the target strings.
+     *
+     * @param source  The string to search within.
+     * @param targets The strings to search for within the source string.
+     * @return true if any of the target strings are found within the source string; false otherwise.
+     */
     public static boolean containsAny(String source, String... targets) {
         if (targets.length < 1) {
             return false;
@@ -224,6 +286,13 @@ public final class StringUtils {
         return false;
     }
 
+    /**
+     * Checks if the source string is equal to any of the target strings, ignoring case.
+     *
+     * @param source the string to compare against the targets
+     * * @param targets an array of strings to be compared with the source
+     * @return true if the source string matches any of the target strings, ignoring case; false otherwise
+     */
     public static boolean equalsAnyIgnoreCase(String source, String... targets) {
         if (targets.length < 1) {
             return false;
@@ -236,6 +305,13 @@ public final class StringUtils {
         return false;
     }
 
+    /**
+     * Checks if the source string is equal to any of the target strings provided.
+     *
+     * @param source the string to be compared against the targets
+     * @param targets variable number of strings to compare with the source
+     * @return true if the source string matches any of the target strings, false otherwise
+     */
     public static boolean equalsAny(String source, String... targets) {
         if (targets.length < 1) {
             return false;
@@ -248,6 +324,13 @@ public final class StringUtils {
         return false;
     }
 
+    /**
+     * Checks if the source string contains all the target strings, ignoring case.
+     *
+     * @param source the string to search in
+     * * @param targets variable number of strings to find within the source
+     * @return true if the source contains all the target strings, ignoring case, false otherwise
+     */
     public static boolean containsAllIgnoreCase(String source, String... targets) {
         if (targets.length < 1) {
             return false;
@@ -260,6 +343,13 @@ public final class StringUtils {
         return true;
     }
 
+    /**
+     * Checks if the source string contains all of the target strings.
+     *
+     * @param source the string to be searched
+     * * @param targets variable number of strings that are expected to be found within the source string
+     * @return true if the source string contains all of the target strings, false otherwise
+     */
     public static boolean containsAll(String source, String... targets) {
         if (targets.length < 1) {
             return false;
@@ -272,6 +362,13 @@ public final class StringUtils {
         return true;
     }
 
+    /**
+     * Checks if two characters are equal ignoring their case.
+     *
+     * @param a the first character to compare
+     * @param b the second character to compare
+     * @return true if both characters are the same when case is ignored, false otherwise
+     */
     public static boolean charEqualIgnoreCase(char a, char b) {
         if (((a >= 65 && a <= 90) || (a >= 97 && a <= 122)) && ((b >= 65 && b <= 90) || (b >= 97 && b <= 122)) && a != b) {
             return Math.abs(a - b) == 32;
@@ -339,10 +436,24 @@ public final class StringUtils {
         return true;
     }
 
+    /**
+     * Removes empty lines from the provided string content.
+     *
+     * @param content the string from which empty lines will be removed
+     * @return a new string with all empty lines removed
+     */
     public static String removeEmptyLine(String content) {
         return content.replaceAll("\\s*\r?\n", NEW_LINE);
     }
 
+    /**
+     * Repeats the given string a specified number of times.
+     * If the number of times is less than or equal to 0, an empty string is returned.
+     *
+     * @param str the string to be repeated
+     * @param times the number of times to repeat the string
+     * @return a new string consisting of the original string repeated multiple times
+     */
     public static String repeat(String str, int times) {
         if (times <= 0) {
             return "";
