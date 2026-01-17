c0730e5497ff19d2b59a810e7b3efd4c558f5e60
chengyuxing <chengyuxingo@gmail.com>
2026-01-17 12:07:55 +0800

    3.1.2

---
+++ src/main/java/org/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains a minimal subset of JetBrains annotations
+ * copied for IDE static analysis only.
+ * <p>
+ * This is a minimal embedded copy, not guaranteed to be in sync with the latest JetBrains annotations.
+ * <p>
+ * DO NOT rely on these annotations at runtime.
+ */
+package org;
